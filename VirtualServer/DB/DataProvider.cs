eb8d83dfacb47ba54894903cb63d231ba680ead6
Александр Пресняков <apresnyakov@alfabank.ru>
2019-05-29 16:45:12 +0300

    ready

--- VirtualServer/DB/DataProvider.cs
+++ VirtualServer/DB/DataProvider.cs
@@ -5,20 +5,16 @@ namespace VirtualServer.DB
     using System.Collections.Specialized;
     using System.Configuration;
 
-    /// <summary>
-    /// Статический класс для подключения к слою доступа к данным
-    /// </summary>
+
     public static class DataProvider
     {
-        /// <summary>
-        /// Леса в домене alfaintra.net — Москва и регионы
-        /// </summary>
+
         internal static StringDictionary adDomains { get; private set; }
 
         #region Строки подключения
 
         /// <summary>
-        /// Строка подключения к UP-SELL
+        /// Строка подключения
         /// </summary>
         private static string upsellcs = ConfigurationManager.ConnectionStrings["MsSqlDB"].ConnectionString;
 
@@ -28,9 +24,7 @@ namespace VirtualServer.DB
 
 
 
-        /// <summary>
-        /// Экземпляр провайдера БД UP-SELL для работы с ЕСПиО
-        /// </summary>
+
         private static Lazy<EspioProvider> espioData = new Lazy<EspioProvider>(() => new EspioProvider(upsellcs), true);
 
 
@@ -39,9 +33,7 @@ namespace VirtualServer.DB
 
         #region Публикация ленивых экземпляров
 
-        /// <summary>
-        /// Провайдер работы с БД UP-SELL при работе с предложениями Еспио
-        /// </summary>
+
         internal static EspioProvider espioProvider
         {
             get
@@ -52,14 +44,7 @@ namespace VirtualServer.DB
 
         #endregion
 
-        /// <summary>
-        /// Инициализация слоя доступа к данным
-        /// </summary>
-        /// <param name="upsellCS">Connection string to UP-SELL db</param>
-        /// <param name="vipModCS">Connection string to VipModule</param>
-        /// <param name="mamsCS">Connection string to MAMS</param>
-        /// <param name="aclubCS">Connection string to AClub</param>
-        /// <param name="domains">Словарь контроллеров домена</param>
+
         public static void Initialize(string upsellCS, StringDictionary domains)
         {
             adDomains = domains;
